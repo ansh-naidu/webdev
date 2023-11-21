554a32e4bbdd68e833d7d0f821ddd32d5467bcac
“Ansh_Naidu” <anshdnaidu@gmail.com>
2023-11-22 03:23:15 +0530

    Updated Inverted Triangle

--- practicals/invertedtriangle.php
+++ practicals/invertedtriangle.php
@@ -1,10 +1,14 @@
 <?php
-$rows = 5; // Number of rows in the inverted triangle
+// Define the number of rows in the inverted triangle
+$numRows = 5;
 
-for ($i = $rows; $i >= 1; $i--) {
+// Generate the inverted triangle pattern
+for ($i = $numRows; $i >= 1; $i--) {
+    // Print asterisks for each row
     for ($j = $i; $j >= 1; $j--) {
         echo "* ";
     }
+    // Move to the next line for the next row
     echo "\n";
 }
 ?>

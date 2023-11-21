a5478d14df4b77a98831faae0a498cff4792c210
“Ansh_Naidu” <anshdnaidu@gmail.com>
2023-11-22 03:24:52 +0530

    Updated factorial

--- practicals/factorial.php
+++ practicals/factorial.php
@@ -1,17 +1,23 @@
 <?php
+// Function to calculate factorial of a number
 function factorial($n) {
+    // Base case: factorial of 0 and 1 is 1
     if ($n === 0 || $n === 1) {
         return 1;
     } else {
         $result = 1;
+
+        // Calculate factorial iteratively
         for ($i = 2; $i <= $n; $i++) {
             $result *= $i;
         }
+
         return $result;
     }
 }
 
-// Calculate factorial of a number (e.g., 5)
+// Example usage to calculate factorial of a number (e.g., 5)
 $number = 5;
 echo "Factorial of " . $number . " is: " . factorial($number);
 ?>
+

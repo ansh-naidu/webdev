f5bf692a7b5d6d6f3dd58be66516aa28f57c737c
“Ansh_Naidu” <anshdnaidu@gmail.com>
2023-11-22 23:29:30 +0530

    Add unit test case for Practical and Song class

--- tests/Unit/Practicaltest.php
+++ tests/Unit/Practicaltest.php
@@ -7,14 +7,31 @@ use PHPUnit\Framework\TestCase;
 require(__DIR__.'/../../practicals/Practical.php');
 use practicals\Practical;
 
-class PracticalTest extends TestCase
-{
-    /**
-     * A basic unit test example.
-     */
-    public function testAdd(): void
-    {
-        $this->assertEquals(Practical::add(1,3), 4);
+class PracticalTest extends TestCase {
+    public function testAddAcceptsFloat() {
+        $this->assertSame(5.0, Practical::add(1.5, 3.5));
+    }
+
+    public function testAddAcceptsFloatInteger() {
+        $this->assertSame(3.5, Practical::add(1.5, 2));
+    }
+
+    public function testAddAcceptsIntegerString() {
+        $this->assertSame(3, Practical::add("1", "2"));
+    }
+
+    public function testAddAcceptsFloatString() {
+        $this->assertSame(5.54, Practical::add("1.4", "3.14"));
+    }
+
+    public function testAddRejectsAlphabetString() {
+        $this->expectException(InvalidArgumentException::class);
+        Practical::add("abc", "xyz");
+    }
+
+    public function testAddRejectsAlphanumericString() {
+        $this->expectException(InvalidArgumentException::class);
+        Practical::add("a1", 1);
     }
 }
 
